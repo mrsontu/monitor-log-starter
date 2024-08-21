4b76f75c9a1a2702b6e620fcf76a7a1d70d2a943
mrsontu <mrsontu.tn@gmail.com>
2024-08-21 15:46:56 +0700

    init

---
+++ FlaskExercise/views.py
@@ -0,0 +1,22 @@
+from flask import flash, render_template, redirect, request
+from FlaskExercise import app
+
+
+@app.route('/')
+def home():
+    log = request.values.get('log_button')
+    if log:
+        if log == 'info':
+          app.logger.info('No issue.')
+        elif log == 'warning':
+         app.logger.warning('Warning occurred.')
+        elif log == 'error':
+         app.logger.error('Error occurred.')
+        elif log == 'critical':
+         app.logger.critical('Critical error occurred.')
+    # TODO: Appropriately log the different button presses
+    #   with the appropriate log level.
+    return render_template(
+        'index.html',
+        log=log
+    )
